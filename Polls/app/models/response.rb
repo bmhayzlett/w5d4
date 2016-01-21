206c9c0c80a71287ef461360792205259131d2f0
Kevin McCall <kmccallsdsu@gmail.com>
2016-01-21 12:05:21 -0800

    Add answer choice, responses, associates question with answer choice

---
+++ Polls/app/models/response.rb
@@ -0,0 +1,13 @@
+class Response < ActiveRecord::Base
+  validates :responder_id, :answer_choice_id, presence: true
+
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: 'AnswerChoice'
+
+  belongs_to :responder,
+    primary_key: :id,
+    foreign_key: :responder_id,
+    class_name: 'User'
+end
--- Polls/db/seeds.rb
+++ Polls/db/seeds.rb
@@ -9,3 +9,11 @@ end
 50.times do
   Question.create!(text: Faker::Hipster.paragraph, poll_id: rand(1..30))
 end
+
+250.times do
+  AnswerChoice.create!(question_id: rand(1..50), answer_text: Faker::Book.title)
+end
+
+500.times do
+  Response.create!(responder_id: rand(1..20), answer_choice_id: rand(1..250))
+end
