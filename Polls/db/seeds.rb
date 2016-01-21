14c390c6868ed3fc7e186d46c78d120e3489823a
Kevin McCall <kmccallsdsu@gmail.com>
2016-01-21 14:00:12 -0800

    Recreate seed file and adds response validations

--- Polls/db/seeds.rb
+++ Polls/db/seeds.rb
@@ -2,18 +2,19 @@
   User.create!(user_name: Faker::Name.name)
 end
 
-30.times do
-  Poll.create!(title: Faker::Hipster.sentence, author_id: rand(1..20))
+User.all.each do |user|
+  Poll.create!(title: Faker::Hipster.sentence, author_id: user.id)
 end
 
-50.times do
-  Question.create!(text: Faker::Hipster.paragraph, poll_id: rand(1..30))
+Poll.all.each do |poll|
+  Question.create!(text: Faker::Hipster.paragraph, poll_id: poll.id)
 end
 
-250.times do
-  AnswerChoice.create!(question_id: rand(1..50), answer_text: Faker::Book.title)
+Question.all.each do |question|
+  AnswerChoice.create!(question_id: question.id, answer_text: Faker::Book.title)
 end
 
-500.times do
-  Response.create!(responder_id: rand(1..20), answer_choice_id: rand(1..250))
+@user_array = *(1..20)
+AnswerChoice.all.each do |answer_choice|
+  Response.create!(responder_id: @user_array.pop , answer_choice_id: answer_choice.id)
 end
