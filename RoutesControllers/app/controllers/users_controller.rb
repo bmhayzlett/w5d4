2ab4d760fcc5c9543587ebd0f0d2a4b3743018cb
Kevin McCall <kmccallsdsu@gmail.com>
2016-01-25 15:33:02 -0800

    implements create and destroy user actions

--- RoutesControllers/app/controllers/users_controller.rb
+++ RoutesControllers/app/controllers/users_controller.rb
@@ -5,19 +5,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def update
-    user = User.update(params[:id], user_params)
-    render json: user
+    @user = User.update(params[:id], user_params)
+    render json: @user
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
     else
       render(
         json: user.errors.full_messages, status: :unprocessable_entity
@@ -26,13 +26,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.destroy(params[:id])
-    render text: 'USER DESTROYED'
+    @user = User.destroy(params[:id])
+    render json: @user
   end
+
+  private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:username)
   end
-
-
 end
